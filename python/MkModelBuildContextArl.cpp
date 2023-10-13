e70525eaec372bb39dec6c0ba1f6184be7546c72
Matthew Ballance <matt.ballance@gmail.com>
2023-10-13 07:33:12 -0700

    Remove obsolete include
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- python/MkModelBuildContextArl.cpp
+++ python/MkModelBuildContextArl.cpp
@@ -1,6 +1,5 @@
 
 #include "zsp/arl/dm/impl/ModelBuildContext.h"
-#include "MkModelBuildContextArl.h"
 
 zsp::arl::dm::IModelBuildContext *mkModelBuildContextArl(zsp::arl::dm::IContext *ctxt) {
     return new zsp::arl::dm::ModelBuildContext(ctxt);

ace055646f04da522cbadda7876a523a3026a026
Matthew Ballance <matt.ballance@gmail.com>
2025-08-27 02:56:59 +0000

    Move back to production ivpm
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -3,7 +3,7 @@
 yum install -y glibc-static ninja-build
 
 echo "BUILD_NUM=${BUILD_NUM}" >> python/zsp_arl_dm/__build_num__.py
-${IVPM_PYTHON} -m pip install ivpm cython setuptools --pre
+${IVPM_PYTHON} -m pip install ivpm cython setuptools
 echo "IVPM version: (1)"
 ${IVPM_PYTHON} -m pip show ivpm
 ${IVPM_PYTHON} -m ivpm update -a --py-prerls-packages
@@ -12,7 +12,7 @@ PYTHON=./packages/python/bin/python
 
 echo "IVPM version: (2)"
 ${PYTHON} -m pip show ivpm
-${PYTHON} -m pip install --upgrade --pre ivpm
+${PYTHON} -m pip install --upgrade ivpm
 echo "IVPM version: (3)"
 ${PYTHON} -m pip show ivpm
 ${PYTHON} -m pip install twine auditwheel ninja wheel cython
